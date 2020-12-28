be19c1ce7952b15062a481908f7238982ec875a2
Aaron Gerig <a.gerig@w-vision.ch>
2020-12-28 15:32:59 +0100

    initial commit

---
+++ src/components/RocketList.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul>
+    <li v-for="rocket in rockets" :key="rocket.id">
+      <h2>{{rocket.name}}</h2>
+      <p>{{rocket.description}}</p>
+      <p><strong>$ {{rocket.costPerLaunch}}</strong></p>
+    </li>
+  </ul>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+const query = gql`
+query getRockets {
+  rockets {
+    id
+    name
+    description
+    costPerLaunch: cost_per_launch
+  }
+}`
+
+export default {
+  name: 'RocketList',
+
+  apollo: {
+    rockets: {
+      query
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
